af87cdd2023630dfc10bfe456dbdbb2e36166e06
angel <amoyapardo@gmail.com>
2018-02-10 17:22:47 +0100

    IMP docker

--- odoo/install.sh
+++ odoo/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp start_odoo.sh.template start_odoo.sh
-cp odoo-server.conf odoo-server.conf.template
+cp etc/odoo-server.conf.template etc/odoo-server.conf
 git clone https://github.com/odoo/odoo.git -b 11.0 --depth 1 --single-branch
 git clone https://github.com/odoo/enterprise.git -b 11.0 --depth 1 --single-branch
